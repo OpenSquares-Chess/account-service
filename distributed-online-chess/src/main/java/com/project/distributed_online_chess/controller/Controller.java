a900dd9e0e3a6f8ebae6237aaf9417fd71cad76c
robbyjunior <robbysandoval111@gmail.com>
2025-09-19 10:24:47 -0700

    Edit 1

--- distributed-online-chess/src/main/java/com/project/distributed_online_chess/controller/Controller.java
+++ distributed-online-chess/src/main/java/com/project/distributed_online_chess/controller/Controller.java
@@ -1,9 +1,14 @@
 package com.project.distributed_online_chess.controller;
 
+import com.project.distributed_online_chess.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
+
+    @Autowired
+    private UserService userService;
 
 
 }

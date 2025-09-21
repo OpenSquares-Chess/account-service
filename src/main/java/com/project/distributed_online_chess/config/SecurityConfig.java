5edade621a1395da04d02d1adfeb1cf3be98a0b7
robbyjunior <robbysandoval111@gmail.com>
2025-09-21 13:44:25 -0700

    Spring Security Update

--- src/main/java/com/project/distributed_online_chess/config/SecurityConfig.java
+++ src/main/java/com/project/distributed_online_chess/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.project.distributed_online_chess.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -11,6 +12,7 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean

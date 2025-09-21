1202b85bb2f45ae3342238a0dc54b008de1353c2
robbyjunior <robbysandoval111@gmail.com>
2025-09-20 17:37:05 -0700

    Spring Authentication

---
+++ src/main/java/com/project/distributed_online_chess/config/SecurityConfig.java
@@ -0,0 +1,42 @@
+package com.project.distributed_online_chess.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/login", "/css/**", "/js/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/home", true)
+                        .permitAll()
+                )
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/login?logout")
+                        .permitAll()
+                );
+        return http.build();
+    }
+    @Bean
+    public UserDetailsService userDetailsService() {
+
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+}
--- src/main/java/com/project/distributed_online_chess/controller/Controller.java
+++ src/main/java/com/project/distributed_online_chess/controller/Controller.java
@@ -2,6 +2,7 @@ package com.project.distributed_online_chess.controller;
 
 import com.project.distributed_online_chess.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,6 +10,16 @@ public class Controller {
 
     @Autowired
     private UserService userService;
+
+    @GetMapping("/public")
+    public String publicEndpoint() {
+        return "Public endpoint.";
+    }
+
+    @GetMapping("/secure")
+    public String secureEndpoint() {
+        return "You are authenticated.";
+    }
 
 
 }

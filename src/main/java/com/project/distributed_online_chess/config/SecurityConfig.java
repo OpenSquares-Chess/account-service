e14c354ee676cb68880b05e84a87b73615b5ffb6
robbyjunior <robbysandoval111@gmail.com>
2025-09-29 22:40:11 -0700

    Code Clean Up

--- src/main/java/com/project/distributed_online_chess/config/SecurityConfig.java
+++ src/main/java/com/project/distributed_online_chess/config/SecurityConfig.java
@@ -12,12 +12,10 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
-public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    http
-            .authorizeHttpRequests(authorize -> authorize
-            .anyRequest().authenticated()
-        )
-        .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
-    return http.build();
-}
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
+                .oauth2ResourceServer((oauth2) ->
+                        oauth2.jwt(Customizer.withDefaults()));
+        return http.build();
+    }
 }
